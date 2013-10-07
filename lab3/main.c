0d804263ed961dce51300ff16a751616d9a8d23d
Kevin Lee <kevinleeone@gmail.com>
2013-10-07 09:51:32 -0500

    Modify error message

--- lab3/main.c
+++ lab3/main.c
@@ -24,29 +24,29 @@ void saveIntoList(const char* object, const char* property, const char* value) {
     if (!cur) { // Object does not exist
         cur = (ObjectNode*) malloc(sizeof(ObjectNode));
         if (!cur) {
-            printf("Insufficient memory");
+            printf("No more memory");
             exit(1);
         }
         cur->objectName = (char*) calloc(strlen(object) + 1, sizeof(char));
         if (!cur->objectName) {
-            printf("Insufficient memory");
+            printf("No more memory");
             exit(1);
         }
         strcpy(cur->objectName, object);
         cur->property = (PropertyNode*) malloc(sizeof(PropertyNode));
         if (!cur->property) {
-            printf("Insufficient memory");
+            printf("No more memory");
             exit(1);
         }
         cur->property->propertyName = (char*) calloc(strlen(property) + 1, sizeof(char));
         if (!cur->property->propertyName) {
-            printf("Insufficient memory");
+            printf("No more memory");
             exit(1);
         }
         strcpy(cur->property->propertyName, property);
         cur->property->value = (char*) calloc(strlen(value) + 1, sizeof(char));
         if (!cur->property->value) {
-            printf("Insufficient memory");
+            printf("No more memory");
             exit(1);
         }
         strcpy(cur->property->value, value);
@@ -56,18 +56,18 @@ void saveIntoList(const char* object, const char* property, const char* value) {
     } else { // Object exist
         PropertyNode* newProperty = (PropertyNode*) malloc(sizeof(PropertyNode)); // Always create a new property
         if (!newProperty) {
-            printf("Insufficient memory");
+            printf("No more memory");
             exit(1);
         }
         newProperty->propertyName = (char*) calloc(strlen(property) + 1, sizeof(char));
         if (!newProperty->propertyName) {
-            printf("Insufficient memory");
+            printf("No more memory");
             exit(1);
         }
         strcpy(newProperty->propertyName, property);
         newProperty->value = (char*) calloc(strlen(value) + 1, sizeof(char));
         if (!newProperty->value) {
-            printf("Insufficient memory");
+            printf("No more memory");
             exit(1);
         }
         strcpy(newProperty->value, value);
