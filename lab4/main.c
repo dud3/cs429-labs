3b0e0a6d85e668fb1000963a0137d7da6994a4fe
Kevin Lee <kevinleeone@gmail.com>
2013-10-19 10:55:29 -0500

    Add addressing modes

--- lab4/main.c
+++ lab4/main.c
@@ -5,50 +5,96 @@
 #define MEMORY_SIZE 4096
 
 typedef struct {
-    int ep;
     int link;
     int programCounter;
     int memory[4096];
 } MachineStatus;
 
+int isHex(char c) {
+    return ('0' <= c && c <= '9') || ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+}
+
+int parseObjectFile(const char* filename, MachineStatus* machineStatus) {
+    int epSet = 0;
+    FILE* obj = fopen(filename, "r"); // Open object file
+    if (!obj) {
+        fprintf(stderr, "Cannot open object file \"%s\"\n", filename);
+        return -1;
+    }
+    while (!feof(obj)) {
+        char buf[1024];
+        fgets(buf, sizeof(buf), obj);
+        if (!strcmp(buf, "\n") || !strcmp(buf, "\r\n")) { // Skip empty line
+            continue;
+        }
+        if (!strncmp(buf, "EP: ", 4)) { // EP: HEX
+            if (epSet == 1 ||
+                    !isHex(buf[4]) ||
+                    !isHex(buf[5]) ||
+                    !isHex(buf[6]) ||
+                    (strcmp(buf + 7, "\n") && strcmp(buf + 7, "\r\n"))) {
+                fprintf(stderr, "Object file error\n> %s", buf);
+                fclose(obj);
+                return -1;
+            }
+            sscanf(buf + 4, "%x", &machineStatus->programCounter);
+            epSet = 1;
+        } else { // HEX: HEX
+            int location;
+            int content;
+            if (!isHex(buf[0]) ||
+                    !isHex(buf[1]) ||
+                    !isHex(buf[2]) ||
+                    !isHex(buf[5]) ||
+                    !isHex(buf[6]) ||
+                    !isHex(buf[7]) ||
+                    strncmp(buf + 3, ": ", 2) ||
+                    (strcmp(buf + 8, "\n") && strcmp(buf + 8, "\r\n"))) {
+                fprintf(stderr, "Object file error\n> %s", buf);
+                fclose(obj);
+                return -1;
+            }
+            sscanf(buf, "%x:%x", &location, &content);
+            machineStatus->memory[location] = content;
+        }
+    }
+    if (!epSet) {
+        fprintf(stderr, "Object file error\n> \"No EP set\"\n");
+        fclose(obj);
+        return -1;
+    }
+    fclose(obj);
+    return 0;
+}
+
+int getMemoryAddress(int instruction, MachineStatus* machineStatus) {
+    int address = instruction & 0x7F;
+    if (instruction & 0x80) { // Current page
+        address |= machineStatus->programCounter & 0x0F80;
+    }
+    if (instruction & 0x0100) { // Indirect addressing
+        address = machineStatus->memory[address];
+    }
+    return address;
+}
+
 int main(int argc, char** argv) {
-    FILE* obj;
-    int epSet;
     MachineStatus* machineStatus;
     if (argc != 2) { // Check syntax
         fprintf(stderr, "Usage: %s object-file\n", argv[0]);
         exit(0);
     }
-    obj = fopen(argv[1], "r"); // Open object file
-    if (!obj) {
-        fprintf(stderr, "Cannot open object file \"%s\"\n", argv[1]);
-        exit(0);
-    }
-    epSet = 0;
     machineStatus = (MachineStatus*) malloc(sizeof(MachineStatus));
     memset(machineStatus, 0, sizeof(MachineStatus));
-    while (!feof(obj)) {
-        char buf[1024];
-        fgets(buf, sizeof(buf), obj);
-        if (!strncmp(buf, "\n", 1) || !strncmp(buf, "\r\n", 2)) { // Skip empty line
-            continue;
-        }
-        if (!strncmp(buf, "EP: ", 4)) {
-            if (epSet == 1 || sscanf(buf, "EP: %x\r\n", &machineStatus->ep) != 1) {
-                fprintf(stderr, "Object file error: %s", buf);
-                free(machineStatus);
-                exit(0);
-            }
-            epSet = 1;
-        }
+    if (parseObjectFile(argv[1], machineStatus)) {
+        free(machineStatus);
+        exit(0);
     }
-    // fscanf(obj, "EP:%x", &machineStatus->ep);
-    // printf("%d\n", machineStatus->ep);
-    // fscanf(obj, "%x:%x", &addr, &instruction);
-    // printf("%x is %x\n", addr, instruction);
-
-
-    fclose(obj);
+    while (1) {
+        int instruction = machineStatus->memory[machineStatus->programCounter];
+        if ((instruction >> 9) <= 5) {
+            getMemoryAddress(instruction, machineStatus);
+    }
     free(machineStatus);
     return 0;
 }
