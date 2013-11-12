fa049e85abff64bbc7976b6bace816e57efaa796
Kevin Lee <kevinleeone@gmail.com>
2013-11-12 13:41:12 -0600

    [lab5] Change output filename

--- lab5/asm8.c
+++ lab5/asm8.c
@@ -465,7 +465,7 @@ int main(int argc, STRING *argv)
                             fprintf (stderr, "Can't open %s\n",*argv);
                             continue;
                         }
-                    char *out_filename = change_file_name(*argv, ".asm", ".obj");
+                    char *out_filename = change_file_name(*argv, ".asm", ".out");
                     output = fopen(out_filename,"w");
                     if (output == NULL)
                         {
