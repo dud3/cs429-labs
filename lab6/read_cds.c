08bcf8c1051b36f44706cf45a7a3fa2e54070660
Kevin Lee <kevinleeone@gmail.com>
2013-11-18 10:48:12 -0600

    [lab6] WIP on read_cds

--- lab6/read_cds.c
+++ lab6/read_cds.c
@@ -1,58 +1,39 @@
-
-/* ***************************************************************** */
-/*                                                                   */
-/*                                                                   */
-/* ***************************************************************** */
-
-/* Reading cache descriptions */
-
 #include "global.h"
 #include "utils.h"
 #include "cds.h"
 
-/* ***************************************************************** */
-/*                                                                   */
-/*                                                                   */
-/* ***************************************************************** */
-
-/* we want to read in a string of some unknown length */
 #define MIN_TOKEN_SIZE 128
 
-struct Token
-{
-    char* string;
-    int    length;
-};
-typedef struct Token Token;
+typedef struct {
+    int length;
+    char* value;
+} Token;
 
-Token *new_token(void)
-{
-    Token *t = (Token*) malloc(sizeof(Token));
+Token* createToken() {
+    Token* t = (Token*) malloc(sizeof(Token));
     t->length = MIN_TOKEN_SIZE;
-    t->string = (char*) malloc(t->length);
+    t->value = (char*) malloc(t->length);
     return t;
 }
 
-void delete_token(Token *t)
-{
-    free(t->string);
+void deleteToken(Token* t) {
+    free(t->value);
     free(t);
 }
 
-void put_char_in_token_at(Token *t, char c, int i)
-{
+void putCharInTokenAt(Token* t, char c, int i) {
     if (i >= t->length)
+    {
+        /* need more space */
+        t->length = 2 * t->length;
+        t->value = (char*) realloc(t->value, t->length);
+        if (t->value == 0)
         {
-            /* need more space */
-            t->length = 2 * t->length;
-            t->string = (char*) realloc(t->string, t->length);
-            if (t->string == 0)
-                {
-                    fprintf(stderr, "Hell has frozen over!!!\n");
-                    exit(-1);
-                }
+            fprintf(stderr, "Hell has frozen over!!!\n");
+            exit(-1);
         }
-    t->string[i] = c;
+    }
+    t->value[i] = c;
 }
 
 
@@ -64,19 +45,19 @@ void get_token(FILE *CDS_file, Token *t)
     int i = 0;
 
     /* token is empty to start */
-    put_char_in_token_at(t, '\0', i);
+    putCharInTokenAt(t, '\0', i);
 
     /* skip spacing, look for first character */
     c = skipBlanks(CDS_file);
     if (c == EOF) return;
 
     while (isalnum(c) || (c == '_'))
-        {
-            put_char_in_token_at(t, c, i);
-            i = i + 1;
-            put_char_in_token_at(t, '\0', i);
-            c = getc(CDS_file);
-        }
+    {
+        putCharInTokenAt(t, c, i);
+        i = i + 1;
+        putCharInTokenAt(t, '\0', i);
+        c = getc(CDS_file);
+    }
 
     /* went one too far, put it back */
     ungetc(c, CDS_file);
@@ -111,10 +92,10 @@ int get_key_value_pair(FILE *CDS_file, Token *key, Token *value)
     c = skipBlanks(CDS_file);
     if (c == EOF) return EOF;
     if ((c != '=') && (c != ':') && (c != '-'))
-        {
-            fprintf(stderr, "not key=value pair: %s %c\n", key->string, c);
-            return EOF;
-        }
+    {
+        fprintf(stderr, "not key=value pair: %s %c\n", key->value, c);
+        return EOF;
+    }
 
     /* we want a second string for the value */
     get_token(CDS_file, value);
@@ -123,17 +104,17 @@ int get_key_value_pair(FILE *CDS_file, Token *key, Token *value)
     c = skipBlanks(CDS_file);
     if (c == EOF) return EOF;
     if ((c != ',') && (c != ';') && (c != '}'))
-        {
-            fprintf(stderr, "not key=value pair: %s %c\n", key->string, c);
-            return EOF;
-        }
+    {
+        fprintf(stderr, "not key=value pair: %s %c\n", key->value, c);
+        return EOF;
+    }
     if (c == '}')
-        {
-            /* we have the last pair, terminated by a '}'.
-               put it back, so that this last pair is processed */
-            ungetc(c, CDS_file);
-            return ',';
-        }
+    {
+        /* we have the last pair, terminated by a '}'.
+           put it back, so that this last pair is processed */
+        ungetc(c, CDS_file);
+        return ',';
+    }
 
     return c;
 }
@@ -141,114 +122,114 @@ int get_key_value_pair(FILE *CDS_file, Token *key, Token *value)
 
 void defineKeyValuePair(CacheDescription* cacheDescription, Token* key, Token* value) {
     if (debug) {
-        fprintf(debugFile, "define %s = %s \n", key->string, value->string);
+        fprintf(debugFile, "define %s = %s \n", key->value, value->value);
     }
     /* look for the name */
-    if (strcasestr(key->string, "name") != 0) {
+    if (strcasestr(key->value, "name") != 0) {
         if (cacheDescription->name != 0) {
             free(cacheDescription->name);
         }
-        cacheDescription->name = allocateString(value->string);
+        cacheDescription->name = allocateString(value->value);
         return;
     }
 
     /* look for line size */
-    if ((strcasestr(key->string, "line") != 0) && (strcasestr(key->string, "size") != 0))
-        {
-            int n = atoi(value->string);
-            cacheDescription->cache->cacheLineSize = n;
-            return;
-        }
+    if ((strcasestr(key->value, "line") != 0) && (strcasestr(key->value, "size") != 0))
+    {
+        int n = atoi(value->value);
+        cacheDescription->cache->cacheLineSize = n;
+        return;
+    }
 
     /* look for number of cache entries */
-    if (strcasestr(key->string, "entries") != 0)
-        {
-            int n = atoi(value->string);
-            cacheDescription->cache->entries = n;
-            return;
-        }
+    if (strcasestr(key->value, "entries") != 0)
+    {
+        int n = atoi(value->value);
+        cacheDescription->cache->entries = n;
+        return;
+    }
 
     /* look for the number of ways */
-    if (strcasestr(key->string, "ways") != 0)
-        {
-            int n = atoi(value->string);
-            cacheDescription->cache->numberOfWays = n;
-            return;
-        }
+    if (strcasestr(key->value, "ways") != 0)
+    {
+        int n = atoi(value->value);
+        cacheDescription->cache->numberOfWays = n;
+        return;
+    }
 
     /* look for write-back */
-    if ((strcasestr(key->string, "write") != 0) && (strcasestr(key->string, "back") != 0))
+    if ((strcasestr(key->value, "write") != 0) && (strcasestr(key->value, "back") != 0))
+    {
+        if (strcasestr(value->value, "true") != 0)
         {
-            if (strcasestr(value->string, "true") != 0)
-                {
-                    cacheDescription->cache->writeBack = 1;
-                    return;
-                }
-            if (strcasestr(value->string, "false") != 0)
-                {
-                    cacheDescription->cache->writeBack = 0;
-                    return;
-                }
+            cacheDescription->cache->writeBack = 1;
+            return;
         }
+        if (strcasestr(value->value, "false") != 0)
+        {
+            cacheDescription->cache->writeBack = 0;
+            return;
+        }
+    }
 
     /* look for write-thru */
-    if ((strcasestr(key->string, "write") != 0) && (strcasestr(key->string, "thru") != 0))
+    if ((strcasestr(key->value, "write") != 0) && (strcasestr(key->value, "thru") != 0))
+    {
+        if (strcasestr(value->value, "true") != 0)
         {
-            if (strcasestr(value->string, "true") != 0)
-                {
-                    cacheDescription->cache->writeBack = 0;
-                    return;
-                }
-            if (strcasestr(value->string, "false") != 0)
-                {
-                    cacheDescription->cache->writeBack = 1;
-                    return;
-                }
+            cacheDescription->cache->writeBack = 0;
+            return;
         }
+        if (strcasestr(value->value, "false") != 0)
+        {
+            cacheDescription->cache->writeBack = 1;
+            return;
+        }
+    }
 
     /* look for the replacement policy */
-    if ((strcasestr(key->string, "policy") != 0) || (strcasestr(key->string, "replace") != 0))
+    if ((strcasestr(key->value, "policy") != 0) || (strcasestr(key->value, "replace") != 0))
+    {
+        if (strcasestr(value->value, "LRU") != 0)
         {
-            if (strcasestr(value->string, "LRU") != 0)
-                {
-                    cacheDescription->cache->replacement_policy = CRP_LRU;
-                    return;
-                }
-            if (strcasestr(value->string, "LFU") != 0)
-                {
-                    cacheDescription->cache->replacement_policy = CRP_LFU;
-                    return;
-                }
-            if (strcasestr(value->string, "random") != 0)
-                {
-                    cacheDescription->cache->replacement_policy = CRP_RANDOM;
-                    return;
-                }
-            if (strcasestr(value->string, "FIFO") != 0)
-                {
-                    cacheDescription->cache->replacement_policy = CRP_FIFO;
-                    return;
-                }
+            cacheDescription->cache->replacement_policy = CRP_LRU;
+            return;
         }
+        if (strcasestr(value->value, "LFU") != 0)
+        {
+            cacheDescription->cache->replacement_policy = CRP_LFU;
+            return;
+        }
+        if (strcasestr(value->value, "random") != 0)
+        {
+            cacheDescription->cache->replacement_policy = CRP_RANDOM;
+            return;
+        }
+        if (strcasestr(value->value, "FIFO") != 0)
+        {
+            cacheDescription->cache->replacement_policy = CRP_FIFO;
+            return;
+        }
+    }
 
     /* look for line size */
-    if ((strcasestr(key->string, "decay") != 0) && (strcasestr(key->string, "interval") != 0)) {
-        int n = atoi(value->string);
+    if ((strcasestr(key->value, "decay") != 0) && (strcasestr(key->value, "interval") != 0)) {
+        int n = atoi(value->value);
         cacheDescription->cache->LFU_Decay_Interval = n;
         return;
     }
-    if (strcasestr(key->string, "ways") != 0) {
-        int n = atoi(value->string);
+    if (strcasestr(key->value, "ways") != 0) {
+        int n = atoi(value->value);
         cacheDescription->cache->numberOfWays = n;
         return;
     }
     // Define victim cache
-    if (strcasestr(key->string, "victim") != 0) {
-        int n = atoi(value->string);
+    if (strcasestr(key->value, "victim") != 0) {
+        int n = atoi(value->value);
         cacheDescription->cache->victimCache.entries = n;
         return;
     }
-    fprintf(stderr, "don't understand %s = %s\n",key->string, value->string);
+    fprintf(stderr, "don't understand %s = %s\n",key->value, value->value);
 }
 
 
@@ -262,9 +243,9 @@ CacheDescription* Read_CDS_file_entry(FILE *CDS_file) {
 
     c = skipBlanks(CDS_file);
     while (c == '#')
-        {
-            c = skipLine(CDS_file);
-        }
+    {
+        c = skipLine(CDS_file);
+    }
     if (c == EOF) return 0;
 
     /* Syntax for Cache Descriptions:  { key=value, key=value, ... } */
@@ -272,10 +253,10 @@ CacheDescription* Read_CDS_file_entry(FILE *CDS_file) {
        cacheDescription defined by the key to have the given value. */
 
     if (c != '{')
-        {
-            fprintf(stderr, "Cache description should start with {, not %c\n", c);
-            return 0;
-        }
+    {
+        fprintf(stderr, "Cache description should start with {, not %c\n", c);
+        return 0;
+    }
 
     /* starting a new cache description.  Get a structure,
        and fill in default values. */
@@ -293,13 +274,13 @@ CacheDescription* Read_CDS_file_entry(FILE *CDS_file) {
     cacheDescription->cache->cacheLine = 0;
     cacheDescription->cache->victimCache.entries = 0;
 
-    Token* key = new_token();
-    Token* value = new_token();
+    Token* key = createToken();
+    Token* value = createToken();
     while (((c = get_key_value_pair(CDS_file, key, value)) != EOF) && (c != '}')) {
         defineKeyValuePair(cacheDescription, key, value);
     }
-    delete_token(key);
-    delete_token(value);
+    deleteToken(key);
+    deleteToken(value);
 
     cacheDescription->cache->name = allocateString(cacheDescription->name);
 
@@ -323,25 +304,25 @@ void readCacheDescriptions(char* CDS_file_name)
     /* open input file */
     CDS_file = fopen(CDS_file_name, "r");
     if (CDS_file == 0)
-        {
-            fprintf (stderr,"Cannot open CDS file %s\n", CDS_file_name);
-        }
+    {
+        fprintf (stderr,"Cannot open CDS file %s\n", CDS_file_name);
+    }
     while ((cacheDescription = Read_CDS_file_entry(CDS_file)) != 0)
+    {
+        /* we use a linked list for all the cache descriptions,
+           but we want to keep the list in the same order tha
+           we read them in.  Bummer. */
+        if (cacheDescriptionRoot == 0)
         {
-            /* we use a linked list for all the cache descriptions,
-               but we want to keep the list in the same order tha
-               we read them in.  Bummer. */
-            if (cacheDescriptionRoot == 0)
-                {
-                    cacheDescriptionRoot = cacheDescription;
-                }
-            else
-                {
-                    CacheDescription*q = cacheDescriptionRoot;
-                    while (q->next != 0) q = q->next;
-                    q->next = cacheDescription;
-                }
-            cacheDescription->next = 0;
+            cacheDescriptionRoot = cacheDescription;
         }
+        else
+        {
+            CacheDescription*q = cacheDescriptionRoot;
+            while (q->next != 0) q = q->next;
+            q->next = cacheDescription;
+        }
+        cacheDescription->next = 0;
+    }
     fclose(CDS_file);
 }
