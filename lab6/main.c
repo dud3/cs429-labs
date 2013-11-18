17dd3620b4121734e54729734fd5989de5600de6
Kevin Lee <kevinleeone@gmail.com>
2013-11-18 10:44:22 -0600

    [lab6] Modify main

--- lab6/main.c
+++ lab6/main.c
@@ -4,36 +4,17 @@ void usage(const char* programName) {
     fprintf(stderr,"Usage: %s [-D] cache-descriptions-file  memory-trace-file\n", programName);
 }
 
-void scanargs(const char* s) {
-    while (*++s) {
-        switch (*s) {
-            case 'D': // Debug
-                debug = 1;
-                if (debug) {
-                    debugFile = fopen("DEBUG_LOG", "w");
-                    if (!debugFile) {
-                        fprintf(stderr, "Cannot open DEBUG_LOG\n");
-                        debug = 0;
-                    }
-                }
-                break;
-            default:
-                fprintf(stderr,"Caches: Bad option %c\n", *s);
-                usage();
-                exit(1);
-        }
-    }
-}
-
 int main(int argc, char** argv) {
-    int i;
-    if (argc != 2) {
+    if (!(argc == 3) && !(argc == 4 && !strcmp(argv[1], "-D"))) {
         usage(argv[0]);
         exit(-1);
     }
-    for (i = 1; i < argc; ++i) {
-        while (**argv == '-') {
-            scanargs(*argv);
+    if (argc == 4) {
+        debug = 1;
+        debugFile = fopen("DEBUG_LOG", "w");
+        if (!debugFile) {
+            fprintf(stderr, "Cannot open DEBUG_LOG\n");
+            debug = 0;
         }
     }
     readCacheDescriptions(argv[0]);
